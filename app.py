fdd340c45789a2999e3c7fdffc5e6c5203b1534a
artyom9912 <artyom9912@gmail.com>
2023-07-01 12:16:46 +1000

    conf

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 from flask import Flask
 import flask_login
 import dash
+import json
 import dash_bootstrap_components as dbc
 import pandas as pd
 from flask_caching import Cache
-# from clickhouse_driver import connect
 from sqlalchemy import create_engine
-# from flaskext.mysql import MySQL
-# con = connect('clickhouse://10.200.2.113')
-engine = create_engine('mysql+pymysql://root:root@127.0.0.1/skameyka')
+
+with open('data.json') as f:
+    config = json.load(f)
+    engine = create_engine(f'mysql+pymysql://{config["username"]}:{config["psswd"]}@{config["ip"]}/{config["db"]}')
 # con = engine.connect()
 app = Flask(__name__)
 app.secret_key = b'_5f#cky2L"F4Q8z]/'
-
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-# app.config['MYSQL_DATABASE_DB'] = 'skameyka'
-# mysql = MySQL()
-# mysql.init_app(app)
-# con = mysql.get_db()
 
 WEEKDAYS= ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
 calendar_cache = []
